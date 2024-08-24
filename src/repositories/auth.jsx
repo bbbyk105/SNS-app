f117df6406fc5be5b7b77ec2ff402bc74c78ffd9
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-08-24 18:11:22 +0900

    signUp機能の追加

--- src/repositories/auth.jsx
+++ src/repositories/auth.jsx
@@ -2,13 +2,13 @@ import { supabase } from "../lib/supabase"
 
 export const authRepository = {
     async signup(name, email, password) {
-        const {data,error} = await supabase.auth.signup({
+        const {data,error} = await supabase.auth.signUp({
             email,
             password,
             options: {data: {name}},
 
         });
-        if(Error != null) throw new Error(error.message);
+        if(error) throw new Error(error.message);
 
         return {...data.user, userName: data.user.user_metadata.name};
     },
